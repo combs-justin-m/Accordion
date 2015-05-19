1fb65c5e4333e9cd9e707adac77aa64f3c6e0a27
Justin Combs <combs.justin.m@gmail.com>
2015-05-19 14:45:32 -0400

    layout finalized

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var $p = $('p');
 
 $('section').on('click', function (){
-  $('p').addClass('hide');
-  $(this).find($p).removeClass('hide');
+  $('.accordion').addClass('hide');
+  $(this).find($('.accordion')).removeClass('hide');
 });
