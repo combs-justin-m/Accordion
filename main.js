b4ecd8cd93b9dd3458795022743c52a39d93f2b1
Justin Combs <combs.justin.m@gmail.com>
2015-05-20 10:18:01 -0400

    accordion js v2

--- main.js
+++ main.js
@@ -2,7 +2,12 @@
 $('.accordion').addClass('hidden');
 $('.accordion:first').removeClass('hidden');
 
-$('section').on('click', function (){
-  $('.accordion').addClass('hidden');
-  $(this).find($('.accordion')).removeClass('hidden');
-});
+// $('section').on('click', function (){
+//   $('.accordion').addClass('hidden');
+//   $(this).find($('.accordion')).removeClass('hidden');
+// });
+
+
+$('main').on('click', 'h2', function(){
+  $(this).next().removeClass('hidden').parent().siblings().find('.accordion').addClass('hidden');
+});`
